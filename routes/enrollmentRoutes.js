9d4e52b868a169339a9ede3a017d0fbcd469b11a
kkomiitlog <hamdannhurbrianne@gmail.com>
2025-12-02 02:16:04 +0800

    Fix import path for enrollmentController

--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { enrollStudent, gradeStudent } from "../controllers/enrollmentController.js";
+import { enrollStudent, gradeStudent } from "./../controllers/enrollmentController.js";
 
 const router = express.Router();
 
