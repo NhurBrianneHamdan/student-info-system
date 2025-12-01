c8bc4ba436eabe0fd831694e7b5223cf1358dee1
kkomiitlog <hamdannhurbrianne@gmail.com>
2025-12-02 00:42:19 +0800

    Delete src directory

--- src/routes/enrollmentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { enrollStudent, gradeStudent } from "../controllers/enrollmentController.js";
-
-const router = express.Router();
-
-router.post('/enroll', enrollStudent);
-router.post('/grade', gradeStudent);
-
-export default router;
